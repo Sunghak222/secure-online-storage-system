0ed5ae2b0f872a6144222136848f2aa6aa040eed
HEO Sunghak <88081894+Sunghak222@users.noreply.github.com>
2025-04-09 17:38:07 +0800

    add otp

--- user_utils.py
+++ user_utils.py
@@ -9,3 +9,14 @@ def get_user_role(username):
     if user_data:
         return user_data[0]
     return None
+
+def get_user_secret(username):
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT secret FROM users WHERE username = ?", (username,))
+    user_secret = cursor.fetchone()
+
+    conn.close()
+
+    return user_secret[0] if user_secret else None
